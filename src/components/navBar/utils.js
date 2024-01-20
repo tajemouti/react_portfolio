7f37cc1ef8411a087bbe3d25616fe45410d88158
juwwariya <tajemouti@gmail.com>
2024-01-20 20:14:22 +0100

    Add utils.js file

---
+++ src/components/navBar/utils.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    label: 'HOME',
+    to: '/',
+  },
+  {
+    label: 'PORTFOLIO',
+    to: '/portfolio',
+  },
+  {
+    label: 'ABOUT ME',
+    to: '/about',
+  },
+  {
+    label: 'SKILLS',
+    to: '/skills',
+  },
+  {
+    label: 'RESUME',
+    to: '/resume',
+  },
+  {
+    label: 'CONTACT',
+    to: '/contact',
+  },
+];
+
+export default data;
