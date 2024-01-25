4f22f6efff692cbfab2e59c0fdf56ec48fef325a
juwwariya <tajemouti@gmail.com>
2024-01-25 15:02:44 +0100

    Implement skills page

---
+++ src/containers/skills/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { FaCode } from 'react-icons/fa';
+import { Animate, AnimateKeyframes } from 'react-simple-animate';
+import { Line } from 'rc-progress';
+import PageHeaderContent from '../../components/pageHeaderContent';
+import skillsData from './utils';
+import './styles.scss';
+import SocialFooter from '../../components/footer';
+
+const Skills = () => (
+  <section id="skills" className="skills">
+    <PageHeaderContent
+      headerText="Skills"
+      icon={<FaCode size={40} />}
+    />
+
+    <div className="skills__content-wrapper">
+      {
+        skillsData.map((item, i) => (
+          <div key={i} className="skills__content-wrapper__inner-content">
+            <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: 'translateX(-200px)',
+              }}
+              end={{
+                transform: 'translateX(0px)',
+              }}
+            >
+              <h3 className="skills__content-wrapper__inner-content__category-text">
+                {item.label}
+              </h3>
+              <div className="progressbar-container">
+                {
+                  item.data.map((skillItem, j) => (
+                    <AnimateKeyframes
+                      key={j}
+                      play
+                      duration={1}
+                      keyframes={['opacity: 1', 'opacity: 0']}
+                      iterationCount="1"
+                    >
+                      <div className="progressbar-wrapper">
+                        <p>{skillItem.skillname}</p>
+                        <Line
+                          percent={skillItem.percentage}
+                          strokeWidth="2"
+                          strokeColor="var(--theme-main-color)"
+                          trailWidth="2"
+                          strokeLinecap="square"
+                        />
+                      </div>
+                    </AnimateKeyframes>
+                  ))
+                }
+              </div>
+            </Animate>
+          </div>
+        ))
+      }
+    </div>
+    <Animate
+      play
+      duration={1}
+      delay={0.3}
+      start={{
+        transform: 'translateX(350px)',
+      }}
+      end={{
+        transform: 'translateX(0px)',
+      }}
+    >
+      <SocialFooter />
+    </Animate>
+  </section>
+);
+
+export default Skills;
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,70 @@
+const skillsData = [
+  {
+    id: 5,
+    label: 'LANGUAGES',
+    data: [
+      {
+        skillname: 'JavaScript',
+        percentage: '75',
+      },
+      {
+        skillname: 'Ruby',
+        percentage: '70',
+      },
+      {
+        skillname: 'HTML5',
+        percentage: '80',
+      },
+      {
+        skillname: 'CSS3',
+        percentage: '80',
+      },
+    ],
+  },
+  {
+    id: 6,
+    label: 'FRAMEWORKS',
+    data: [
+      {
+        skillname: 'React.js',
+        percentage: '75',
+      },
+      {
+        skillname: 'Ruby on Rails',
+        percentage: '80',
+      },
+      {
+        skillname: 'RSpec',
+        percentage: '85',
+      },
+      {
+        skillname: 'Capybara',
+        percentage: '80',
+      },
+    ],
+  },
+  {
+    id: 7,
+    label: 'TOOLS',
+    data: [
+      {
+        skillname: 'Database Management',
+        percentage: '80',
+      },
+      {
+        skillname: 'API Design',
+        percentage: '80',
+      },
+      {
+        skillname: 'Version Control',
+        percentage: '85',
+      },
+      {
+        skillname: 'CLI',
+        percentage: '90',
+      },
+    ],
+  },
+];
+
+export default skillsData;
