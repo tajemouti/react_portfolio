f08e098777a018dc3d0ffb9f4398edc9f58e2450
juwwariya <tajemouti@gmail.com>
2024-01-25 15:01:12 +0100

    Implement contact page

---
+++ src/containers/contact/index.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { Animate } from 'react-simple-animate';
+import PageHeaderContent from '../../components/pageHeaderContent';
+import './styles.scss';
+import SocialFooter from '../../components/footer';
+
+const Contact = () => (
+  <section id="contact" className="contact">
+    <PageHeaderContent
+      headerText="Contact"
+      icon={<AiOutlineMail size={40} />}
+    />
+    <div className="contact__content">
+      <Animate
+        play
+        duration={1}
+        delay={0}
+        start={{
+          transform: 'translateX(-200px)',
+        }}
+        end={{
+          transform: 'translateX(0px)',
+        }}
+      >
+        <h3 className="contact__content__header-text">I&apos;m always interested in hearing about new projects, so if you&apos;d like to chat please get in touch.</h3>
+      </Animate>
+      <Animate
+        play
+        duration={1}
+        delay={0}
+        start={{
+          transform: 'translateX(200px)',
+        }}
+        end={{
+          transform: 'translateX(0px)',
+        }}
+      >
+        <form className="contact__content__form" action="https://formspree.io/f/mdovqere" method="post">
+          <div className="contact__content__form__controlswrapper">
+            <div>
+              <input
+                required
+                name="name"
+                id="name"
+                className="inputName"
+                type="text"
+              />
+              <label htmlFor="name" className="nameLabel">
+                Name
+              </label>
+            </div>
+            <div>
+              <input
+                required
+                name="email"
+                id="email"
+                className="inputEmail"
+                type="email"
+              />
+              <label htmlFor="email" className="emailLabel">
+                Email
+              </label>
+            </div>
+            <div>
+              <textarea
+                required
+                name="description"
+                id="description"
+                className="inputDescription"
+                type="text"
+                rows="5"
+              />
+              <label htmlFor="description" className="descriptionLabel">
+                Description
+              </label>
+            </div>
+          </div>
+          <button type="submit">GET IN TOUCH</button>
+        </form>
+      </Animate>
+    </div>
+
+    <Animate
+      play
+      duration={1}
+      delay={0}
+      start={{
+        transform: 'translateX(-200px)',
+      }}
+      end={{
+        transform: 'translateX(0px)',
+      }}
+    >
+      <SocialFooter />
+    </Animate>
+  </section>
+);
+
+export default Contact;
