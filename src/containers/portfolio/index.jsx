94c1c63474dbdbd6b14d5390cf0e7205ed8a59f3
tajemouti <tajemouti@gmail.com>
2024-02-16 23:44:55 +0100

    Add transition to project section

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineProject } from 'react-icons/ai';
+import { Animate } from 'react-simple-animate';
 import PageHeaderContent from '../../components/pageHeaderContent';
 import './styles.scss';
 import { filterData, portfolioData } from './utils';
@@ -25,53 +26,64 @@ const Portfolio = () => {
         headerText="Portfolio"
         icon={<AiOutlineProject size={40} />}
       />
-
-      <div className="portfolio__content">
-        <ul className="portfolio__content__filter">
-          {filterData.map((item) => (
-            <li
-              className={item.filterId === filteredValue ? 'active' : ''}
-              key={item.filterId}
-              onClick={() => handleFilter(item.filterId)}
-            >
-              {item.label}
-            </li>
-          ))}
-        </ul>
-        <div className="portfolio__content__cards">
-          {
-            filteredItems.map((item, index) => (
-              <div
-                key={item.name.trim()}
-                className="portfolio__content__cards__item"
-                onMouseEnter={() => handleHover(index)}
-                onMouseLeave={() => handleHover(null)}
+      <Animate
+        play
+        duration={1.5}
+        delay={0.5}
+        start={{
+          transform: 'translateY(770px)',
+        }}
+        end={{
+          transform: 'translateY(0px)',
+        }}
+      >
+        <div className="portfolio__content">
+          <ul className="portfolio__content__filter">
+            {filterData.map((item) => (
+              <li
+                className={item.filterId === filteredValue ? 'active' : ''}
+                key={item.filterId}
+                onClick={() => handleFilter(item.filterId)}
               >
-                <div className="portfolio__content__cards__item__img-wrapper">
-                  <img src={item.image} alt="" />
+                {item.label}
+              </li>
+            ))}
+          </ul>
+          <div className="portfolio__content__cards">
+            {
+              filteredItems.map((item, index) => (
+                <div
+                  key={item.name.trim()}
+                  className="portfolio__content__cards__item"
+                  onMouseEnter={() => handleHover(index)}
+                  onMouseLeave={() => handleHover(null)}
+                >
+                  <div className="portfolio__content__cards__item__img-wrapper">
+                    <img src={item.image} alt="" />
+                  </div>
+                  <div className="overlay">
+                    {
+                      index === hoveredValue && (
+                        <div>
+                          <h4>{item.name}</h4>
+                          <p>{item.description}</p>
+                          <ul>
+                            {item.tech.map((techItem, techIndex) => (
+                              <li key={techIndex}>{techItem}</li>
+                            ))}
+                          </ul>
+                          <a href={item.link} rel="noopener noreferrer" target="_blank">Visit</a>
+                          <a href={item.code} rel="noopener noreferrer" target="_blank">Code</a>
+                        </div>
+                      )
+                    }
+                  </div>
                 </div>
-                <div className="overlay">
-                  {
-                    index === hoveredValue && (
-                      <div>
-                        <h4>{item.name}</h4>
-                        <p>{item.description}</p>
-                        <ul>
-                          {item.tech.map((techItem, techIndex) => (
-                            <li key={techIndex}>{techItem}</li>
-                          ))}
-                        </ul>
-                        <a href={item.link} rel="noopener noreferrer" target="_blank">Visit</a>
-                        <a href={item.code} rel="noopener noreferrer" target="_blank">Code</a>
-                      </div>
-                    )
-                  }
-                </div>
-              </div>
-            ))
-          }
+              ))
+            }
+          </div>
         </div>
-      </div>
+      </Animate>
     </section>
   );
 };
